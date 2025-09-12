8532d879fc37b39d39e6651aa135143bbe6693e1
a1io6 <bilihobs@icloud.com>
2025-09-12 14:17:50 +0600

    buttu okshoit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages үчүн base './', локалдык иштетүүдө да туура иштейт
 export default defineConfig({
-  base: './', 
+  base: './', // Бул relative path кылат
   plugins: [react()],
 })
