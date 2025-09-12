e004fcdb7c34d01cc3f6235c016b4aaaaf4529a6
a1io6 <bilihobs@icloud.com>
2025-09-12 14:12:16 +0600

    main ondoldu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// GitHub Pages үчүн base './', локалдык иштетүүдө да туура иштейт
 export default defineConfig({
+  base: './', 
   plugins: [react()],
 })
